0e84f149d26fc7706d66c498637d524376c1faec
Wesley Marques <Marques_bertin@hotmail.com>
2024-10-17 00:43:00 -0300

    feat: adicionado allure report, tags, description nos testes

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,13 @@
-module.exports = {
+import { allureCypress } from "allure-cypress/reporter";
+
+export default {
   e2e: {
     baseUrl:'https://bugbank.netlify.app',
     setupNodeEvents(on, config) {
+      allureCypress(on, config, {
+        resultsDir: "allure-results",
+      });
+      return config;
     },
   },
 };
